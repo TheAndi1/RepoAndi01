d78d41276821b84620ac17d2846a3dd83c453f14
TheAndi1 <andreas.jakobi@meine-krankenkasse.de>
2024-12-13 10:17:17 +0100

    calculation of wrapping paper and gift ribbon

--- day01_solution.py
+++ day01_solution.py
@@ -1,31 +1,34 @@
 from operator import index
 
-datei = open('day01_input.txt','r')
+datei = None
+try: #leitet ExceptionListening ein
+    datei = open('day01_input.txt','r')
 
-#for zeile in datei:
-#	print("Inhalt aus Datei: ")
-#	print(zeile)
+    #for zeile in datei:
+    #	print("Inhalt aus Datei: ")
+    #	print(zeile)
 
-#print(datei.read())
+    #print(datei.read())
 
-#print(len(datei.read()))
-counter = 0
-#position = 0
-ausgabe = False
+    #print(len(datei.read()))
+    counter = 0
+    #position = 0
+    ausgabe = False
 
 
-for index, zeichen in enumerate(datei.read()):
-    #position += 1
-    if zeichen == "(":
-        counter += 1
+    for index, zeichen in enumerate(datei.read()):
+        #position += 1
+        if zeichen == "(":
+            counter += 1
 
-    elif zeichen == ")":
-        counter -= 1
+        elif zeichen == ")":
+            counter -= 1
 
-    if counter == -1 and ausgabe == False:
-        print(index+1)
-        #print(position)
-        ausgabe = True
+        if counter == -1 and ausgabe == False:
+            print(index+1)
+            #print(position)
+            ausgabe = True
 
-print(counter)
-datei.close()
+    print(counter)
+finally: #auch bei Exceptions kommt er hier vorbei
+    datei.close()
--- day02_solution.py
+++ day02_solution.py
@@ -1,9 +1,35 @@
 from operator import index
 
-datei = open('day02_input.txt','r')
 
-for zeile in datei:
-    zeile = zeile.replace('\n', '')
-    werte = zeile.split('x')
-    flaeche=werte[0]
-    print(werte)
+datei = None
+try: #leitet ExceptionListening ein
+    datei = open('day02_input.txt','r')
+    gesamtflaeche = 0
+    gesamtband = 0
+
+    for zeile in datei:
+        zeile = zeile.replace('\n', '') #in jeder Zeile \n (Zeilenumbruch rausschmeißen)
+        werte = zeile.split('x') #mittels der SPLIT Funktion die drei Werte in eine Liste übertragen, Zeile für Zeile
+
+        #GeschenkpapierBerechnung
+        flaeche = (2 * (int(werte[0]) * int(werte[1]))) + (2 * (int(werte[0]) * int(werte[2]))) + (2 * (int(werte[1]) * int(werte[2])))
+        zusatz = min((int(werte[0]) * int(werte[1])), (int(werte[0]) * int(werte[2])), (int(werte[1]) * int(werte[2])))
+
+        #GeschenkbandBerechnung
+        kleinste = min(int(werte[0]), int(werte[1]), int(werte[2]))
+        mitte = int(werte[0]) + int(werte[1]) + int(werte[2]) - min(int(werte[0]), int(werte[1]), int(werte[2])) - max(int(werte[0]), int(werte[1]), int(werte[2]))
+        groesste = max(int(werte[0]), int(werte[1]), int(werte[2]))
+
+        band = kleinste + kleinste + mitte + mitte
+        schleife = kleinste * mitte * groesste
+
+
+        #finale Berechnungen
+        gesamtflaeche = gesamtflaeche + flaeche + zusatz
+        gesamtband = gesamtband + band + schleife
+
+    print(gesamtflaeche) #Part One der Aufgabe
+    print(gesamtband) #Part Two der Aufgabe
+
+finally: #auch bei Exceptions kommt er hier vorbei
+    datei.close()
