7b7048a2bff3c5077e8f90e92fb1de1b4fb862f3
TheAndi1 <andreas.jakobi@meine-krankenkasse.de>
2024-12-16 12:08:43 +0100

    replace if then else by table lookup (dict)

--- day01_solution.py
+++ day01_solution.py
@@ -1,18 +1,19 @@
 from operator import index
 from pathlib import Path
+import sys
 
 content = Path('day01_input.txt').read_text()
 #an dieser Stelle ist nicht mal mehr die Datei offen
+
+mapping = {
+    "(" : +1,
+    ")": -1,
+}
 counter = 0
 ausgabe = False
-
-
 for index, zeichen in enumerate(content):
-    if zeichen == "(":
-        counter += 1
 
-    elif zeichen == ")":
-        counter -= 1
+    counter += mapping[zeichen]
 
     if counter == -1 and ausgabe == False:
         print(index+1)
