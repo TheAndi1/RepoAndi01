90b7cdcfbb261388275ec55e516b3998eef789e8
TheAndi1 <andreas.jakobi@meine-krankenkasse.de>
2025-10-01 07:07:33 +0200

    second part of day06

--- day06_KI_solution_with_regular_expressions.py
+++ day06_KI_solution_with_regular_expressions.py
@@ -25,7 +25,7 @@ def parse_line(line: str):
 def run(instructions):
     lights = [[False]*1000 for _ in range(1000)]
 
-    for line in instructions:
+    for line in instructions
         action, (x1, y1), (x2, y2) = parse_line(line)
         for x in range(x1, x2+1):
             for y in range(y1, y2+1):
